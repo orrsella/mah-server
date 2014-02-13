86d32b84cfdaa24ba29404b4d5ee2dc5312340f4
Orr Sella <orr@sella.org>
2014-02-13 22:38:52 +0200

    Update ghost module

--- puppet/modules/ghost/manifests/init.pp
+++ puppet/modules/ghost/manifests/init.pp
@@ -19,10 +19,17 @@ class ghost {
     # ghost version
     $version = '0.4.1'
 
-    file { "ghost-$version.zip":
-        ensure => present,
-        path => "/opt/ghost-$version/",
-        source => "http://ghost.org/zip/ghost-$version.zip",
-        require => Class['nodejs']
+    # file { "ghost-$version.zip":
+    #     ensure => present,
+    #     path => "/opt/ghost-$version/",
+    #     source => "http://ghost.org/zip/ghost-$version.zip",
+    #     require => Class['nodejs']
+    # }
+
+    wget::fetch { "http://ghost.org/zip/ghost-$version.zip":
+        destination => "/opt/ghost-$version/ghost-$version.zip",
+        timeout     => 0,
+        verbose     => false,
+        require     => Class['nodejs']
     }
 }
