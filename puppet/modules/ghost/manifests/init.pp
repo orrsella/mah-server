cb927090fdeeb025db5d04576865393ee761ed52
Orr Sella <orr@sella.org>
2014-02-14 00:05:55 +0200

    Update ghost module

--- puppet/modules/ghost/manifests/init.pp
+++ puppet/modules/ghost/manifests/init.pp
@@ -1,15 +1,5 @@
 class ghost {
 
-    # package { 'nodejs':
-    #     ensure => present
-    # }
-
-    # Install semver, required by nodejs installer
-    # package { 'semver':
-    #     ensure   => installed,
-    #     provider => gem,
-    # }
-
     # Install nodejs
     class { 'nodejs':
         version => 'v0.10.25',
@@ -19,19 +9,12 @@ class ghost {
     # ghost version
     $version = '0.4.1'
 
-    file { "/opt/ghost-$version/":
+    file { "/opt/ghost/":
         ensure  => directory
     }
 
-    # wget::fetch { "http://ghost.org/zip/ghost-$version.zip":
-    #     destination => "/opt/ghost-$version/ghost-$version.zip",
-    #     timeout     => 0,
-    #     verbose     => false,
-    #     require     => File["/opt/ghost-$version/"]
-    # }
-
     # hack to only extract the archive it ghost doesn't already exist
-    $ghost = file("/opt/ghost-$version/package.json", '/dev/null')
+    $ghost = file('/opt/ghost/package.json', '/dev/null')
 
     if ($ghost == '') {
         archive { "ghost-$version":
@@ -39,25 +22,32 @@ class ghost {
             url       => "http://ghost.org/zip/ghost-$version.zip",
             extension => 'zip',
             checksum  => false,
-            target    => "/opt/ghost-$version",
-            require   => File["/opt/ghost-$version/"],
+            target    => '/opt/ghost',
+            require   => File['/opt/ghost/'],
             notify    => Exec['/usr/local/node/node-default/bin/npm install --production']
         }
     }
 
-    # include nodejs
-
-    # nodejs::npm { "/opt/ghost-$version:ghost":
-    #     ensure      => present,
-    #     install_opt => '--production',
-    #     require     => Archive["ghost-$version"]
-    #     # version => '2.5.9',
-    # }
-
     exec { '/usr/local/node/node-default/bin/npm install --production':
-        cwd         => "/opt/ghost-$version",
-        # subscribe   => Archive["ghost-$version"],
+        cwd         => '/opt/ghost',
         refreshonly => true,
-        # onlyif      =>
+    }
+
+    file { '/opt/ghost/config.js':
+        ensure  => present,
+        source  => 'puppet:///modules/ghost/config.js',
+        require => File['/opt/ghost/']
+    }
+
+    file { '/etc/init/ghost.conf':
+        ensure => present,
+        source => 'puppet:///modules/ghost/ghost.conf'
+    }
+
+    service { 'ghost':
+        ensure    => running,
+        enable    => true,
+        subscribe => File['/opt/ghost/config.js'],
+        require   => File['/etc/init/ghost.conf']
     }
 }
