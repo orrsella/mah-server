f175133842cc9cb5cef8443566fab1abb9bcf98c
Orr Sella <orr@sella.org>
2014-02-13 22:41:22 +0200

    Update ghost module

--- puppet/modules/ghost/manifests/init.pp
+++ puppet/modules/ghost/manifests/init.pp
@@ -19,17 +19,14 @@ class ghost {
     # ghost version
     $version = '0.4.1'
 
-    # file { "ghost-$version.zip":
-    #     ensure => present,
-    #     path => "/opt/ghost-$version/",
-    #     source => "http://ghost.org/zip/ghost-$version.zip",
-    #     require => Class['nodejs']
-    # }
+    file { "/opt/ghost-$version/":
+        ensure  => directory
+    }
 
     wget::fetch { "http://ghost.org/zip/ghost-$version.zip":
         destination => "/opt/ghost-$version/ghost-$version.zip",
         timeout     => 0,
         verbose     => false,
-        require     => Class['nodejs']
+        require     => File["/opt/ghost-$version/"]
     }
 }
--- setup.sh
+++ setup.sh
@@ -39,4 +39,4 @@ echo "Install puppet modules"
 puppet module install netmanagers/fail2ban
 puppet module install attachmentgenie/ufw
 puppet module install willdurand/nodejs
-puppet module install maestrodev/wget
+# puppet module install maestrodev/wget
