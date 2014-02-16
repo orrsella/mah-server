d9ca8d62dddee54e3a2b33cb940693a4378680c8
Orr Sella <orr@sella.org>
2014-02-17 01:35:51 +0200

    Update ghost manifest

--- puppet/modules/ghost/manifests/init.pp
+++ puppet/modules/ghost/manifests/init.pp
@@ -48,7 +48,7 @@ class ghost {
     exec { '/usr/bin/npm install --production':
         cwd         => '/opt/ghost',
         refreshonly => true,
-        require     => [File['/usr/bin/npm'], Archive["ghost-$ghost_ver"]]
+        # require     => [File['/usr/bin/npm'], Archive["ghost-$ghost_ver"]]
     }
 
     file { '/opt/ghost/config.js':
